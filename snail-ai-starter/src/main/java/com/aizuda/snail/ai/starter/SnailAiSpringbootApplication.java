beac96219aa1395e073b91da53d1a15ea5536c22
绥焰儿 <775525420@qq.com>
2026-06-17 04:40:05 +0000

    !12 feat(0.0.6): Storage支持业务库多数据库适配
    * feat(0.0.6): Storage支持业务库多数据库适配

--- snail-ai-starter/src/main/java/com/aizuda/snail/ai/starter/SnailAiSpringbootApplication.java
+++ snail-ai-starter/src/main/java/com/aizuda/snail/ai/starter/SnailAiSpringbootApplication.java
@@ -1,6 +1,5 @@
 package com.aizuda.snail.ai.starter;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +11,6 @@ import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = "com.aizuda.snail.ai")
 @EnableConfigurationProperties
-@MapperScan("com.aizuda.snail.ai.persistence")
 @EnableAsync
 @SpringBootConfiguration
 @EnableScheduling
