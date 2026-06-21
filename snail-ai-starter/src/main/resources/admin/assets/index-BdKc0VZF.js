4d63dd31657f43409f3f7e4cdd70ee9216e603a9
opensnail <598092184@qq.com>
2026-06-21 11:52:07 +0800

    feat(0.0.6): admin 打包

---
+++ snail-ai-starter/src/main/resources/admin/assets/index-BdKc0VZF.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./exception-base.vue_vue_type_script_setup_true_lang-D8ri3dhL.js";import{d as n,o as t,c as a}from"./index-B0Y05iQo.js";const m=n({name:"404",__name:"index",setup(c){return(_,s)=>{const e=o;return t(),a(e,{type:"404"})}}});export{m as default};
